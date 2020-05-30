f05738d79cb9e7ba2c34e8a6216cb68e8a6b7e5a
Keicee32 <kelss451@gmail.com>
2020-05-30 13:31:14 -0700

    more commits

---
+++ src/EstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using EstateApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstateApp.Data.DatabaseContexts.AuthenticationDbContext
+{
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+         : base(options)
+         {
+             
+         }
+    }
+}
